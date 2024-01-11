f814aeec7922811705513a29362437e3748ebafc
guaru345 <abelmtz34@outlook.com>
2024-01-11 16:31:49 -0600

    add most important project and create component to show laboral experience

---
+++ src/utils/constants.ts
@@ -0,0 +1,37 @@
+export const PROJECTS = [
+  {
+    name: "GuarUtify",
+    descriptions: [
+      {
+        paragraph: `El proyecto Spotify Clone es un proyecto interesante que toma como
+          base el diseño de Midudev y se distingue por su enfoque especializado en
+          el desarrollo del backend para el frontend (BFF). Esta aplicación brinda
+          a los usuarios una experiencia completa y personalizada para disfrutar
+          de la música`,
+      },
+      {
+        paragraph: `En el corazón del proyecto se encuentra la capacidad de reproducir
+          música de manera fluida, ofreciendo una interfaz intuitiva que permite a
+          los usuarios explorar, descubrir y disfrutar de sus canciones favoritas.
+          Además de la reproducción de música, la aplicación permite a los
+          usuarios construir y gestionar sus propias playlists personalizadas,
+          creando así una experiencia auditiva única y adaptada a sus
+          preferencias.`,
+      },
+      {
+        paragraph: `La funcionalidad se extiende aún más al ofrecer a los usuarios la
+          posibilidad de construir su propia biblioteca musical personal,
+          facilitando el acceso rápido a sus artistas y álbumes favoritos.`,
+      },
+      {
+        paragraph: ` Con una interfaz de usuario pulida y funciones robustas, el proyecto
+          "Spotify Clone" se presenta como una opción atractiva y completa para
+          aquellos que buscan una experiencia musical personalizada y envolvente.`,
+      },
+    ],
+    image: {
+      src: "/spotify.png",
+      alt: "image of project spotify clone",
+    },
+  },
+];
