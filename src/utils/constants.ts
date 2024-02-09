af388e230cc9b337c12dea1dc26206026cef929f
guarU345 <abelmtz34@outlook.com>
2024-02-09 13:50:37 -0600

    fix gramatical text

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,15 +1,6 @@
 export const PROJECTS = [
   {
     name: "GuarUtify",
-    descriptions: [
-      {
-        paragraph: `El proyecto Spotify Clone es un proyecto interesante que toma como
-          base el diseño de Midudev y se distingue por su enfoque especializado en
-          el desarrollo del backend para el frontend (BFF). Esta aplicación brinda
-          a los usuarios una experiencia completa y personalizada para disfrutar
-          de la música`,
-      },
-    ],
     image: {
       src: "/guarutify.webp",
       alt: "image of project spotify clone",
@@ -18,28 +9,23 @@ export const PROJECTS = [
   },
 ];
 
+
 export const JOBS = [
   {
-    title: "Practicante - Zaro Transportation",
-    date: "Mayo 2022 - Nov 2022",
-    activities: `Practique y participe en el desarrollo de un sistema web encargado de
-    administrar los fermentadores de una cerveceria todo esto mediante los datos adquiridos
-    de 3 sensores.`
+    "title": "Practicante - Zaro Transportation",
+    "date": "Mayo 2022 - Nov 2022",
+    "activities": "Practiqué y participé en el desarrollo de un sistema web encargado de administrar los fermentadores de una cervecería, todo esto mediante los datos adquiridos de 3 sensores."
   },
   {
-    title: "Desarrollador web Odoo - Jarsa",
-    date: "Ene 2023 - Abr 2023",
-    activities: `Lleve a cabo el curso oficial para entendimiento y creacion de modulos
-    en el erp Odoo, ademas desarrolle modulos tanto pequeños como grandes para modulos
-    custom que ofrecia la empresa ya sea el sistema de contabilidad mediante cfdi 4.0
-    y modificaciones a los modulos oficiales.`
+    "title": "Desarrollador web Odoo - Jarsa",
+    "date": "Ene 2023 - Abr 2023",
+    "activities": "Llevé a cabo el curso oficial para el entendimiento y creación de módulos en el ERP Odoo. Además, desarrollé módulos tanto pequeños como grandes para módulos custom que ofrecía la empresa, ya sea el sistema de contabilidad mediante CFDI 4.0 y modificaciones a los módulos oficiales."
   },
   {
-    title: "Desarrollador Full-Stack - SW Argos",
-    date: "Mayo 2023 - Ene 2024",
-    activities: `Desarrolle un apartado en el cual se registrarian las incidencias
-    generadas en cada viaje de las unidades cada incidencia se podia crear,modificar e
-    incluso cerrar, tambien desarrolle 2 apartados para el sistema administrativo de la 
-    empresa que se encargaba de los depositos y facturas.`
+    "title": "Desarrollador Full-Stack - SW Argos",
+    "date": "Mayo 2023 - Ene 2024",
+    "activities": "Desarrollé un apartado en el cual se registrarían las incidencias generadas en cada viaje de las unidades. Cada incidencia se podía crear, modificar e incluso cerrar. También desarrollé 2 apartados para el sistema administrativo de la empresa, que se encargaba de los depósitos y facturas."
   }
 ]
+
+
