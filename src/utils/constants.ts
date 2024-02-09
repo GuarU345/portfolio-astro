63c2c76e3eaa8faae129a11b3604d5e2b3727893
guarU345 <abelmtz34@outlook.com>
2024-02-09 01:00:12 -0600

    add new section to show jobs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,8 +11,35 @@ export const PROJECTS = [
       },
     ],
     image: {
-      src: "/spotify.webp",
+      src: "/guarutify.webp",
       alt: "image of project spotify clone",
     },
+    url: 'https://guarutify.netlify.app'
   },
 ];
+
+export const JOBS = [
+  {
+    title: "Practicante - Zaro Transportation",
+    date: "Mayo 2022 - Nov 2022",
+    activities: `Practique y participe en el desarrollo de un sistema web encargado de
+    administrar los fermentadores de una cerveceria todo esto mediante los datos adquiridos
+    de 3 sensores.`
+  },
+  {
+    title: "Desarrollador web Odoo - Jarsa",
+    date: "Ene 2023 - Abr 2023",
+    activities: `Lleve a cabo el curso oficial para entendimiento y creacion de modulos
+    en el erp Odoo, ademas desarrolle modulos tanto pequeños como grandes para modulos
+    custom que ofrecia la empresa ya sea el sistema de contabilidad mediante cfdi 4.0
+    y modificaciones a los modulos oficiales.`
+  },
+  {
+    title: "Desarrollador Full-Stack - SW Argos",
+    date: "Mayo 2023 - Ene 2024",
+    activities: `Desarrolle un apartado en el cual se registrarian las incidencias
+    generadas en cada viaje de las unidades cada incidencia se podia crear,modificar e
+    incluso cerrar, tambien desarrolle 2 apartados para el sistema administrativo de la 
+    empresa que se encargaba de los depositos y facturas.`
+  }
+]
